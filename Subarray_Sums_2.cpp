ecec1989e5bea90bb46c470c248a59ae1f06db17
tanish35 <tanishmajumdar2912@gmail.com>
2024-10-20 00:01:38 +0530

    First commit

---
+++ Subarray_Sums_2.cpp
@@ -0,0 +1,193 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#define ll long long
+#define vll vector<long long>
+#define db double
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pb push_back
+#define mp make_pair
+#define sorta(a) sort(a.begin(), a.end())
+#define sortd(a) sort(a.begin(), a.end(), greater<int>())
+#define vpll vector<pair<ll, ll>>
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define pq priority_queue<ll>
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+#define umap unordered_map<ll, ll, custom_hash>
+#define uset unordered_set<ll, custom_hash>
+
+ll pow(ll base, ll exponent, ll modulus)
+{
+    ll result = 1;
+    base = base % modulus;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+        {
+            result = (result * base) % modulus;
+        }
+        exponent = exponent >> 1;
+        base = (base * base) % modulus;
+    }
+    return result;
+}
+
+// Print function without newline
+template <typename T>
+void prints_helper(const T &t)
+{
+    cout << t;
+}
+
+template <typename T, typename... Args>
+void prints_helper(const T &t, const Args &...args)
+{
+    cout << t;
+    prints_helper(args...);
+}
+
+template <typename... Args>
+void prints(const Args &...args)
+{
+    prints_helper(args...);
+}
+
+// Print function with newline
+
+template <typename T>
+void print_helper(const T &t)
+{
+    cout << t << " ";
+}
+
+template <typename T>
+void print_helper(const vector<T> &v)
+{
+    cout << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            cout << ", ";
+        cout << *it;
+    }
+    cout << "]";
+}
+
+template <typename T>
+void print_helper(const set<T> &s)
+{
+    cout << "{";
+    for (auto it = s.begin(); it != s.end(); ++it)
+    {
+        if (it != s.begin())
+            cout << ", ";
+        cout << *it;
+    }
+    cout << "}";
+}
+
+template <typename T>
+void print_helper(const unordered_set<T, custom_hash> &us)
+{
+    cout << "{";
+    for (auto it = us.begin(); it != us.end(); ++it)
+    {
+        if (it != us.begin())
+            cout << ", ";
+        cout << *it;
+    }
+    cout << "}";
+}
+
+template <typename K, typename V>
+void print_helper(const map<K, V> &m)
+{
+    cout << "{";
+    for (auto it = m.begin(); it != m.end(); ++it)
+    {
+        if (it != m.begin())
+            cout << ", ";
+        cout << it->first << ": " << it->second;
+    }
+    cout << "}";
+}
+
+template <typename K, typename V>
+void print_helper(const unordered_map<K, V, custom_hash> &um)
+{
+    cout << "{";
+    for (auto it = um.begin(); it != um.end(); ++it)
+    {
+        if (it != um.begin())
+            cout << ", ";
+        cout << it->first << ": " << it->second;
+    }
+    cout << "}";
+}
+
+template <typename T, typename... Args>
+void print_helper(const T &t, const Args &...args)
+{
+    print_helper(t);
+    print_helper(args...);
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+    print_helper(args...);
+    cout << endl;
+}
+
+void setIO(string s)
+{
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll n, x;
+    cin >> n >> x;
+    vll a(n);
+    vll prefix(n + 1, 0);
+    umap m;
+    m[0] = 1;
+    ll ans = 0;
+    forn(i, n)
+    {
+        cin >> a[i];
+        prefix[i + 1] = prefix[i];
+        prefix[i + 1] += a[i];
+        ans += m[prefix[i + 1] - x];
+        m[prefix[i + 1]]++;
+    }
+    cout << ans << endl;
+    return 0;
+}
